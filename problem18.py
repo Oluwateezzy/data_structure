b4872a53cdf62a3cc90ec7bb1a60199392323e30
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-01-03 12:41:51 +0100

    update

--- problem18.py
+++ problem18.py
@@ -33,7 +33,7 @@ After 4 seconds, word becomes equal to "abcbabcd" and reverts to its initial sta
 It can be shown that 4 seconds is the minimum time greater than zero required for word to revert to its initial state.
 """
 
-class Solution:
+class Solution3:
   def z_function(self, s):
     z, l, r, n = [0] * len(s), 0, 0, len(s)
     for i in range(1, n):
@@ -55,5 +55,101 @@ class Solution:
       if z[i] == n - i:
         return res
       res += 1
-
+    print(res)
     return res
+  
+  
+
+#   class Solution:
+#     def minimumTimeToInitialState(self, word: str, k: int) -> int:
+#         z = [0] * len(word)
+#         res = 1
+
+#         if word == len(word)*word[0]:
+#             return res
+
+#         for i in range(1, len(word)):
+#             count = 0
+#             for j in range(len(word) - i):
+#                 if word[i + j] == word[j]:
+#                     count += 1
+#                 else:
+#                     break
+#             z[i] = count
+            
+#         for i in range(k, len(word), k):
+#             if z[i] == len(word) - i:
+#                 return res
+#             res += 1
+#         return res
+        
+# class Solution:
+#     def minimumTimeToInitialState(self, word: str, k: int) -> int:
+#         if word == len(word)*word[0]:
+#             return 1
+#         if len(word) % k != 0:
+#             return 1
+#         return len(word) // k
+
+# class Solution:
+#     def minimumTimeToInitialState(self, word: str, k: int) -> int:
+#         t = 0
+#         while True:
+#             scrambled = self.doF(word, k)
+#             t = t + 1
+#             if scrambled == word:
+#                 return t
+#             word = scrambled
+    
+#     def doF(self, word, k):
+#         first_k = word[:k]
+#         remaining = word[k:]
+#         return remaining + first_k
+    
+#     abacaba
+#     caba
+#     [0, 0, 1, 0, 3, 0, 1]
+
+#     [0, 0, 1, 0, 3, 0, 1]
+#     [0, 0, 1, 0, 3, 0, 1]
+#     [0, 0, 1, 0, 3, 0, 1]
+#     [0, 0, 0, 0, 3, 0, 0, 0]
+#     [0, 0, 0, 0, 3, 0, 0, 0]
+
+
+# class Solution:
+#     def minimumTimeToInitialState(self, word: str, k: int) -> int:
+#         n = len(word)
+#         res = 1
+#         current = word
+        
+#         # Perform the operations until the string reverts
+#         while True:
+#             # Remove first k characters and append them to the end
+#             current = current[k:] + current[:k]
+#             if current == word:
+#                 return res
+#             res += 1
+
+class Solution2:
+    def minimumTimeToInitialState(self, word: str, k: int) -> int:
+        res = 1
+        z = [0] * len(word)
+        
+        for i in range(1, len(word)):
+            count = 0
+            for j in range(len(word) - i):
+                if word[i + j] == word[j]:
+                    count += 1
+                else:
+                    break
+            z[i] = count
+            
+        for i in range(k, len(word), k):
+            if z[i] == len(word) - i:
+                return res
+            res += 1
+        return res
+
+s = Solution2()
+print(s.minimumTimeToInitialState("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 1))
