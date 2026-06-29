97e1ad8285da5a34fce512b3921f9597e3f65d16
Oluwatobiloba <oluwateezzy03@gmail.com>
2026-06-29 11:41:34 +0100

    feat: implement binary search approach to solve minimum largest delivery time problem

--- problem36.py
+++ problem36.py
@@ -1,14 +1,30 @@
 def minimumLargestDeliveryTime(delivery_times, k):
-    x = len(delivery_times) // k
-    arr = []
+    left = max(delivery_times)
+    right = sum(delivery_times)
+    print(f"left {left}, right {right}")
 
-    if x == 1:
-        return max(delivery_times)
-    
-    for i in range(x):
-        arr.append(sum(delivery_times[i : x + 1]))
+    while left < right:
+        mid = (left + right) // 2
+        print(f"mid {mid}")
 
-    return max(arr)
+        partitions = 1
+        current_sum = 0
+
+        for time in delivery_times:
+            print(f"current sum {current_sum} \n")
+            if current_sum + time > mid:
+                partitions += 1
+                current_sum = time
+            else:
+                current_sum += time
+        
+
+        if partitions <= k:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
 
 print(minimumLargestDeliveryTime([7,2,5,10,8], 2))
 print(minimumLargestDeliveryTime([1,4,4], 3))
